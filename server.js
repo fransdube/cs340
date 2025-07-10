197bfafffc12d1716c9098df0392778803edcd05
FM Dube <fransdube@gmail.com>
2025-07-11 00:22:54 +0200

    database

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
 
 
 /* ***********************
@@ -28,7 +29,8 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 app.get("/", (req, res) => {
-  res.render("index", { title: "Home" })
+  // res.render("index", { title: "Home" })
+  app.get("/", baseController.buildHome)
 })
 
 
